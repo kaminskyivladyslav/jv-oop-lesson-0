2e2989a77dc923823072e5a123dc3f5f44879a9c
Kaminskyi <kaminskyiwladyslaw@gmail.com>
2026-01-07 21:56:34 +0100

    fixed unnecessary code

--- src/main/java/mate/academy/service/AuthenticationService.java
+++ src/main/java/mate/academy/service/AuthenticationService.java
@@ -9,24 +9,8 @@ public class AuthenticationService {
         this.userService = new UserService();
     }
 
-    /**
-     * Imagine that some user wants to login to your site.
-     * You should check if user credentials (login and password) are valid or not.
-     * All users are stored in <code>UserService</code> class.
-     *
-     * @param email    - user's email
-     * @param password - user's password
-     * @return true if user by email exists and passed password is equal to user's password.
-     * Return false in any other cases.
-     */
     public boolean login(String email, String password) {
         User user = userService.findByEmail(email);
-        if (user == null || email == null || password == null) {
-            return false;
-        } else if (user.getPassword().equals(password)) {
-            return true;
-        } else {
-            return false;
-        }
+        return user != null && user.getPassword().equals(password);
     }
 }
